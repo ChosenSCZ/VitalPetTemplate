cb169281f642173a40abf698b8f6e768596d1f65
ChosenSCZ <christoffer.espinozah@gmail.com>
2024-03-23 21:33:04 -0400

    close

---
+++ add_cart_v4-main/add_cart_v4-main/products.js
@@ -0,0 +1,131 @@
+const products = [
+    {
+        "id": 1,
+        "name": "BARF Guau! 1Kg",
+        "flavor": "Res",
+        "description": "1Kg de delicioso Alimento BARF Guau! sabor Res",
+        "price": 40,
+        "image": "assets/img/barf-guau-res.png"
+    },  
+    {
+        "id": 2,
+        "name": "BARF Guau! 5Kg",
+        "flavor": "Res",
+        "description": "5Kg de delicioso Alimento BARF Guau! sabor Res",
+        "price": 195,
+        "image": "assets/img/barf-guau-res.png"
+    },  
+    {
+        "id": 3,
+        "name": "BARF Guau! 10Kg",
+        "flavor": "Res",
+        "description": "10Kg de delicioso Alimento BARF Guau! sabor Res",
+        "price": 380,
+        "image": "assets/img/barf-guau-res.png"
+    },
+    {
+        "id": 4,
+        "name": "BARF Guau! 1Kg",
+        "flavor": "Pollo",
+        "description": "1Kg de delicioso Alimento BARF Guau! sabor Pollo",
+        "price": 38,
+        "image": "assets/img/barf-guau-pollo.png"
+    },
+    {
+        "id": 5,
+        "name": "BARF Guau! 5Kg",
+        "flavor": "Pollo",
+        "description": "5Kg de delicioso Alimento BARF Guau! sabor Pollo",
+        "price": 185,
+        "image": "assets/img/barf-guau-pollo.png"
+    },
+    {
+        "id": 6,
+        "name": "BARF Guau! 10Kg",
+        "flavor": "Pollo",
+        "description": "10Kg de delicioso Alimento BARF Guau! sabor Pollo",
+        "price": 360,
+        "image": "assets/img/barf-guau-pollo.png"
+    },
+    {
+        "id": 7,
+        "name": "BARF Miau! 1Kg",
+        "flavor": "Res",
+        "description": "1Kg de delicioso Alimento BARF Miau! sabor Res",
+        "price": 40,
+        "image": "assets/img/barf-miau-res.png"
+    },
+    {
+        "id": 8,
+        "name": "BARF Miau! 3Kg",
+        "flavor": "Res",
+        "description": "3Kg de delicioso Alimento BARF Miau! sabor Res",
+        "price": 115,
+        "image": "assets/img/barf-miau-res.png"
+    },
+    {
+        "id": 9,
+        "name": "BARF Miau! 5Kg",
+        "flavor": "Res",
+        "description": "5Kg de delicioso Alimento BARF Miau! sabor Res",
+        "price": 190,
+        "image": "assets/img/barf-miau-res.png"
+    },
+    {
+        "id": 10,
+        "name": "BARF Miau! 1Kg",
+        "flavor": "Pollo",
+        "description": "1Kg de delicioso Alimento BARF Miau! sabor Pollo",
+        "price": 38,
+        "image": "assets/img/barf-miau-pollo.png"
+    },
+    {
+        "id": 11,
+        "name": "BARF Miau! 3Kg",
+        "flavor": "Pollo",
+        "description": "3Kg de delicioso Alimento BARF Miau! sabor Pollo",
+        "price": 110,
+        "image": "assets/img/barf-miau-pollo.png"
+    },
+    {
+        "id": 12,
+        "name": "BARF Miau! 5Kg",
+        "flavor": "Pollo",
+        "description": "5Kg de delicioso Alimento BARF Miau! sabor Pollo",
+        "price": 180,
+        "image": "assets/img/barf-miau-pollo.png"
+    },
+    {
+        "id": 13,
+        "name": "GummyPet",
+        "flavor": "Pequeño",
+        "description": "14 porciones pequeñas de gomitas de colageno 100% naturales",
+        "price": 30,
+        "image": "assets/img/gummypetp.png"
+    },
+    {
+        "id": 14,
+        "name": "GummyPet",
+        "flavor": "Grande",
+        "description": "7 porciones grandes de gomitas de colageno 100% naturales",
+        "price": 30,
+        "image": "assets/img/gummypetg.png"
+    },
+    {
+        "id": 15,
+        "name": "GummyPet 4x3",
+        "flavor": "Pequeño",
+        "description": "56 porciones pequeñas de gomitas de colageno 100% naturales",
+        "price": 90,
+        "image": "assets/img/gummypetp.png"
+    },
+    {
+        "id": 16,
+        "name": "GummyPet 4x3",
+        "flavor": "Grande",
+        "description": "28 porciones grandes de gomitas de colageno 100% naturales",
+        "price": 90,
+        "image": "assets/img/gummypetg.png"
+    }
+];
+export default products;
